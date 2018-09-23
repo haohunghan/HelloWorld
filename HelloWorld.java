75e1fdadade613c99361f128711b4c15d25c497c
haohunghan <43365889+haohunghan@users.noreply.github.com>
2018-09-23 16:12:20 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world");
+		System.out.println("Hello world I am Hao");
 	}
 
 }
