a47f81e6f59fd993b7842614a1262fe2a831d4e2
haohunghan <43365889+haohunghan@users.noreply.github.com>
2018-09-25 09:04:32 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,9 +3,26 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
+		/*Scanner sc = new Scanner(System.in);
 		String name = sc.next();
-		System.out.println("Hello world I am " + name);
+		System.out.println("Hello world I am " + name);*/
+		static String filename = "Input your filename here";
+    		public static void main(String[] args) {
+		System.out.println("Hello world I'm TDTU");
+		int count= 0;
+		try{
+		    Scanner sc = new Scanner(new FileReader(new File(filename)) );
+		    while (sc.hasNext()){
+			String line = sc.next();
+			System.out.println("Helo world, I'm " + line);
+			count++;
+		    }
+
+		} catch(IOException e){
+
+		}
+		System.out.println("Total " + count + " students");
+    }
 	}
 
 }
