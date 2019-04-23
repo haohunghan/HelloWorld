f3387b285abca776b2c8d0c31fca5ed519e21993
haohunghan <43365889+haohunghan@users.noreply.github.com>
2019-04-23 18:37:04 +0700

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,31 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author STUDENT TDTU
+ */
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
-public class HelloWorld {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		/*Scanner sc = new Scanner(System.in);  
-		String name = sc.next();  
-		System.out.println("Hello world I am " + name);*/
-		static String filename = "Input your filename here";
-    		public static void main(String[] args) { 
-		System.out.println("Hello world I'm TDTU");
-		int count= 0;
-		try{
-		    Scanner sc = new Scanner(new FileReader(new File(filename)) );
-		    while (sc.hasNext()){
-			String line = sc.next();
-			System.out.println("Helo world, I'm " + line);
-			count++;
-		    }
-
-		} catch(IOException e){
-
-		}
-		System.out.println("Total " + count + " students");
-    }  
-	} 
-
+public class NaiveBayesClassifier {
+    
+    
+    
+    public static void main(String[] args) throws FileNotFoundException{
+        
+        Scanner sc = new Scanner(new File("data/tennis.txt"));
+        String[] type = new String[5]; 
+        String strTemp;
+        int count = 0;
+        ArrayList<String> dataArrL = new ArrayList<>();
+        
+        while (sc.hasNextLine()){
+            
+            if (count == 0){
+                strTemp = sc.nextLine();
+                String[] tmp = strTemp.split(",");
+                
+                for (int i=0; i< tmp.length; i++){
+                    type[i] = tmp[i]; 
+                }
+            }
+            else {
+                dataArrL.add(sc.nextLine());
+            }
+            
+            count++;
+           // strTemp = sc.nextLine();
+           // String[] tmp = strTemp.split(",");
+        }
+        
+        ArrayList<String> keys = new ArrayList<>();
+        for (int i=0; i< dataArrL.size(); i++){
+            
+            String[] tmp = dataArrL.get(i).split(",");
+            for (int j=0; j < tmp.length; i++){
+                keys.add(tmp[j]);
+            }
+        }
+        System.out.println(keys);
+             
+    }
+    
 }
-  
- 
-  
